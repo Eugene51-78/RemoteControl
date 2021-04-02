392dba68a963f5a52a6458278a2f6ef8885243c6
Ubuntu <ubuntu@ip-172-31-28-228.us-east-2.compute.internal>
2021-04-02 22:37:38 +0000

    LoginSerializer

--- RemoteControl/authentication/views.py
+++ RemoteControl/authentication/views.py
@@ -5,8 +5,8 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import RegistrationSerializer
-
+from .serializers import RegistrationSerializer, LoginSerializer
+from .renderers import UserJSONRenderer
 # Create your views here.
 
 class RegistrationAPIView(APIView):
@@ -15,6 +15,7 @@ class RegistrationAPIView(APIView):
     """
     permission_classes = (AllowAny,)
     serializer_class = RegistrationSerializer
+    renderer_classes = (UserJSONRenderer,)
 
     def post(self, request):
         user = request.data.get('user', {})
@@ -26,3 +27,19 @@ class RegistrationAPIView(APIView):
         serializer.save()
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class LoginAPIView(APIView):
+    permission_classes = (AllowAny,)
+    renderer_classes = (UserJSONRenderer,)
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        user = request.data.get('user', {})
+
+        # Обратите внимание, что мы не вызываем метод save() сериализатора, как
+        # делали это для регистрации. Дело в том, что в данном случае нам
+        # нечего сохранять. Вместо этого, метод validate() делает все нужное.
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
