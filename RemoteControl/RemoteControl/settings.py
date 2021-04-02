6c5bdd8f79211a4ce39b8ebad24a8ccff580b021
Ubuntu <ubuntu@ip-172-31-28-228.us-east-2.compute.internal>
2021-04-02 22:57:04 +0000

    Exceptions Handler

--- RemoteControl/RemoteControl/settings.py
+++ RemoteControl/RemoteControl/settings.py
@@ -127,3 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'EXCEPTION_HANDLER': 'RemoteControl.exceptions.core_exception_handler',
+    'NON_FIELD_ERRORS_KEY': 'error',
+}
